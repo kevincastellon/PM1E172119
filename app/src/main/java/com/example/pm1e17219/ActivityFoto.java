4ff0a0b116a0ea5cb479e70870c060ac214bab09
kevincastellon <kevincast256@gmail.com>
2024-02-17 11:21:43 -0600

    Examen

--- app/src/main/java/com/example/pm1e17219/ActivityFoto.java
+++ app/src/main/java/com/example/pm1e17219/ActivityFoto.java
@@ -11,7 +11,7 @@ public class ActivityFoto extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vfoto);
+        setContentView(R.layout.activity_foto);
         imgGuardada = (ImageView) findViewById(R.id.ImgGuardada);
     }
 
